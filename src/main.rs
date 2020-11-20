c271c0cf269e5ab64378579bdc6ed0eda4e715e8
Zeno <zeno@oxydyze.dev>
2020-11-20 13:14:50 -0500

    Sand now falls at a constant 1 cell/frame

--- src/main.rs
+++ src/main.rs
@@ -60,7 +60,12 @@ impl Grid {
                 let y = (idx / self.width) as i32;
 
                 canvas.set_draw_color(material.color());
-                canvas.draw_point(Point::new(x, y));
+                let rect = Rect::from_center(
+                    Point::new(x, y).scale(PIXEL_SIZE as i32),
+                    PIXEL_SIZE as u32,
+                    PIXEL_SIZE as u32
+                );
+                canvas.fill_rect(rect);
             }
         }
     }
@@ -89,7 +94,22 @@ pub struct Sand;
 
 impl Material for Sand {
     fn update(&self, grid: &Grid, position: Point) -> Point {
-        position
+        let down = position.offset(0, 1);
+        if down.y >= grid.height as i32 { return position; }
+        let down_left = position.offset(-1, 1);
+        if down_left.x < 0 { return position; }
+        let down_right = position.offset(1, 1);
+        if down_right.x >= grid.width as i32 { return position; }
+        
+        if grid[down].is_none() {
+            down
+        } else if grid[down_left].is_none() {
+            down_left
+        } else if grid[down_right].is_none() {
+            down_right
+        } else {
+            position
+        }
     }
     
     fn color(&self) -> Color {
@@ -119,14 +139,15 @@ impl Clone for Box<Material> {
     }
 }
 
-const WIDTH: usize = 800;
-const HEIGHT: usize = 600;
+const WIDTH: usize = 1920;
+const HEIGHT: usize = 1024;
+const PIXEL_SIZE: u8 = 5;
 
 pub fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
  
-    let mut grid = Grid::new(WIDTH, HEIGHT);
+    let mut grid = Grid::new(WIDTH / PIXEL_SIZE as usize, HEIGHT / PIXEL_SIZE as usize);
 
     let window = video_subsystem.window("rust-sdl2 demo", WIDTH as u32, HEIGHT as u32)
         .position_centered()
@@ -153,10 +174,14 @@ pub fn main() {
 
         let mouse = event_pump.mouse_state();
         if(mouse.left()) {
-            let cursor = Point::new(mouse.x(), mouse.y());
+            let cursor = Point::new(
+                mouse.x() / PIXEL_SIZE as i32, 
+                mouse.y() / PIXEL_SIZE as i32);
             grid[cursor] = Some(Box::new(Sand));
         }
         // The rest of the game loop goes here...
+
+        grid.update();
 
         canvas.set_draw_color(Color::RGB(0, 0, 0));
         canvas.clear();
